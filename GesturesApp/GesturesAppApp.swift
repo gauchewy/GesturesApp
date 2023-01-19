0e29267b8da891185b145e821c61b7f12e6c955b
gauchewy <90925919+gauchewy@users.noreply.github.com>
2023-01-19 17:09:56 -0500

    initial commit

---
+++ GesturesApp/GesturesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesAppApp.swift
+//  GesturesApp
+//
+//  Created by Qiwei on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct GesturesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
