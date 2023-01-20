a03809af3110b503519538650c4bf2b2e14104a3
gauchewy <90925919+gauchewy@users.noreply.github.com>
2023-01-20 17:21:43 -0500

    most basic spotlight

--- GesturesApp/Views/ContentView.swift
+++ GesturesApp/Views/ContentView.swift
@@ -8,16 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPressed = false
+
     var body: some View {
-        VStack {
+        ZStack {
             Image("frog")
                 .resizable()
                 .scaledToFit()
 
+            Rectangle()
+                .fill(Color.black)
+                .opacity(isPressed ? 0.2 : 1)
+                .gesture(
+                    LongPressGesture(minimumDuration: 0.1)
+                        .onEnded{ _ in
+                            self.isPressed.toggle()
+                        }
+                )
         }
         .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
