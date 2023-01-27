6098f3d6ad0a8109304d039a490a07f2438ae668
gauchewy <90925919+gauchewy@users.noreply.github.com>
2023-01-27 15:58:22 -0500

    model

---
+++ GesturesApp/DisplayManager.swift
@@ -0,0 +1,14 @@
+//
+//  DisplayManager.swift
+//  GesturesApp
+//
+//  Created by Qiwei on 1/27/23.
+//
+
+import Foundation
+
+struct DisplayImage {
+    var pressed: Bool
+    
+    func show(_ pressed: Bool)
+}
